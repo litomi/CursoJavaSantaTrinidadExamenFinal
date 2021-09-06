2359d0481758ba52601fa2f33a51c53b8481da82
litomi <47027713+litomi@users.noreply.github.com>
2021-09-06 04:37:35 -0300

    Resolución examen Java

--- Ejercicios/Ejercio6.sql
+++ Ejercicios/Ejercio6.sql
@@ -1,14 +1,4 @@
 SELECT
-    bria.ganancias AS "Ganancias",
-    bria.cantVentas AS "Cantidad de ventas",
-    COUNT(*) AS "Cantidad de bicicletas en venta"
-FROM bicicleteria AS bria 
-INNER JOIN bicicleta AS bta
-ON bria.idBicicleteria = bta.Bicicleteria_idBicicleteria
-WHERE bria.idBicicleteria = 2;
-
-
-SELECT
     ganancias AS "Ganancias",
     cantVentas AS "Cantidad de ventas",
     COUNT(*) AS "Cantidad de bicicletas en venta"
